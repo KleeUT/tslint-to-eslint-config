bf9b3595102844cb3c978430d771b80abc88249d
kleeut <klee.ut@gmail.com>
2019-10-06 21:25:14 +1100

    :bug: passes parameters to end to end test

--- test/createTestArgs.ts
+++ test/createTestArgs.ts
@@ -6,11 +6,13 @@ const readdir = promisify(fs.readdir);
 
 export const createTestArgs = async (cwd: string, extraArgs: string[]) => {
     const items = new Set(await readdir(cwd));
-    const flags = ["--config", path.join(cwd, ".eslintrc.json")];
+    const flags = [["--config", `"${path.join(cwd, ".eslintrc.json")}"`]];
 
     if (items.has("tslint.json")) {
-        flags.push("--tslint", path.join(cwd, "tslint.json"));
+        flags.push(["--tslint", `"${path.join(cwd, "tslint.json")}"`]);
     }
 
-    return [...flags.map(flag => `"${flag}"`).join(" "), ...extraArgs];
+    const allFlags = [...flags.map(flag => `${flag.join(" ")}`), ...extraArgs];
+
+    return allFlags;
 };
